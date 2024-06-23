ef502dd53f671488456517aabc875600976e4662
vzsoares <vinicius.zenha@gmail.com>
2024-06-23 18:08:11 -0300

    chore: remove base layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,14 +7,14 @@ import { Inter } from 'next/font/google';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: `TypeScript starter for Next.js by João Pedro Schmitz`,
-  description: `TypeScript starter for Next.js that includes all you need to build amazing apps`,
+    title: `Nextjs Ultimate SaaS`,
+    description: `The SaaS tamplate you needed`,
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body className={inter.className}>{children}</body>
+        </html>
+    );
 }
