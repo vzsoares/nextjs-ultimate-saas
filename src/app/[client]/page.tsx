82bf93337eaa4a2637d495ffb61a123156fa727e
vzsoares <vinicius.zenha@gmail.com>
2024-06-24 21:17:31 -0300

    feat: base homepage

--- src/app/[client]/page.tsx
+++ src/app/[client]/page.tsx
@@ -1,6 +1,27 @@
-import { PageProps } from '../types';
+import { ClientSchema } from '@/schemas';
+import { Clients, PageProps } from '../../types';
 
 export default function Page(p: PageProps) {
-    console.log(p.params.client);
-    return null;
+    const client = p.params.client;
+    ClientSchema.parse(client);
+    return HomeFactory(client);
+}
+
+export function HomeFactory(client: Clients) {
+    return (
+        <main className="min-h-screen flex-col flex">
+            <nav className="px-4 py-2 w-full max-w-[1080px] bg-red-500 m-auto">
+                <p className="font-medium text-lg">{client}</p>
+            </nav>
+            <section className="px-4 w-full py-2 max-w-[1080px] m-auto flex-1">
+                <p className="text-2xl mt-8 text-center">
+                    Welcome too <span className="text-4xl">{client}</span>
+                </p>
+                <div>todo</div>
+            </section>
+            <footer className="px-4 py-2 w-full max-w-[1080px] bg-red-500 m-auto">
+                <p className="text-center">2024 ©</p>
+            </footer>
+        </main>
+    );
 }
--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,6 @@
+export type Clients = 'foo' | 'bar' | 'baz';
+
 export type PageProps<P = object, T = object> = {
     params: { client: Clients } & P;
     searchParams: Record<string, string>;
 } & T;
-
-export type Clients = 'foo' | 'bar' | 'baz';
