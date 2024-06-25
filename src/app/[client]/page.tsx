1ae58da8c70e01075b1eae51d3cefb2a3ff185d8
vzsoares <vinicius.zenha@gmail.com>
2024-06-24 21:42:50 -0300

    feat: svgr

--- src/app/[client]/page.tsx
+++ src/app/[client]/page.tsx
@@ -1,5 +1,7 @@
+import MainIllustration from 'public/innovation_illustration.svg';
+
 import { ClientSchema } from '@/schemas';
-import { Clients, PageProps } from '../../types';
+import { Clients, PageProps } from '@/types';
 
 export default function Page(p: PageProps) {
     const client = p.params.client;
@@ -17,7 +19,12 @@ export function HomeFactory(client: Clients) {
                 <p className="text-2xl mt-8 text-center">
                     Welcome too <span className="text-4xl">{client}</span>
                 </p>
-                <div>todo</div>
+                <div className="text-red-500 max-w-[500px] m-auto pt-12">
+                    <MainIllustration />
+                </div>
+                <div>
+                    <p className="text-center pt-4 text-medium text-lg">Seeking innovation</p>
+                </div>
             </section>
             <footer className="px-4 py-2 w-full max-w-[1080px] bg-red-500 m-auto">
                 <p className="text-center">2024 ©</p>
