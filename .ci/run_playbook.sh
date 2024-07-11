d98bd051c184101da3aa7b41c7f551181f7236e6
vzsoares <vinicius.zenha@gmail.com>
2024-07-11 19:40:03 -0300

    fix: mv

--- .ci/run_playbook.sh
+++ .ci/run_playbook.sh
@@ -1,8 +1,8 @@
-source ./.env
+source ../.env
 export UBUNTU_PASS
 export AWS_REGION
 export AWS_ACCOUNT_ID
 export AWS_ACCESS_KEY_ID
 export AWS_SECRET_ACCESS_KEY
 # ansible-playbook -i inventory.yaml playbook.yaml
-ansible-playbook -i inventory.yaml playbook_docker.yaml --extra-vars "HOST=localubuntu" -vvv
+ansible-playbook -i inventory.ansible.yaml playbook_docker.ansible.yaml --extra-vars "HOST=localubuntu" -vvv
